33eb890b7ba7992c746b533504731b82947f8229
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-07-09 10:41:07 +0300

    Moving the Sync ALl Task to The tasks directory

--- api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SyncAllJob.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SyncAllJob.java
@@ -1,12 +1,12 @@
-package org.openmrs.module.ugandaemrsync;
+package org.openmrs.module.ugandaemrsync.tasks;
 
 import org.openmrs.module.ugandaemrsync.server.SyncDataRecord;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
 /**
- *
+ *A Task to Sync all data to the central server.
  */
-public class SyncJob extends AbstractTask {
+public class SyncAllJob extends AbstractTask {
 	
 	public void execute() {
 		SyncDataRecord syncDataRecord = new SyncDataRecord();
