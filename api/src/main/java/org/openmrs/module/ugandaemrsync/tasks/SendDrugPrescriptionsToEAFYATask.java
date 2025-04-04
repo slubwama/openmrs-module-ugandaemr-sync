6dbf7e9c846bdc0fcdb8fb5da327dda149828b45
slubwama1 <lubwamasamuel3@gmail.com>
2025-04-04 05:12:51 +0300

    remove unused imports

--- api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SendDrugPrescriptionsToEAFYATask.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/tasks/SendDrugPrescriptionsToEAFYATask.java
@@ -1,19 +1,12 @@
 package org.openmrs.module.ugandaemrsync.tasks;
 
-import netscape.javascript.JSObject;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.json.JSONObject;
-import org.openmrs.Concept;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.ugandaemrsync.api.UgandaEMRSyncService;
-import org.openmrs.module.ugandaemrsync.model.SyncTaskType;
 import org.openmrs.module.ugandaemrsync.server.SyncFHIRRecord;
 import org.openmrs.scheduler.tasks.AbstractTask;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class SendDrugPrescriptionsToEAFYATask extends AbstractTask {
 
