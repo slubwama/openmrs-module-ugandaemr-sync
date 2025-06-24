297df4d51cbf71a734ceb7842266e3cb63dcedfc
slubwama1 <lubwamasamuel3@gmail.com>
2025-06-24 08:10:25 +0300

    improve dependenies in UgandaEMR sync for the Jackson

--- api/src/main/java/org/openmrs/module/ugandaemrsync/api/UgandaEMRHttpURLConnection.java
+++ api/src/main/java/org/openmrs/module/ugandaemrsync/api/UgandaEMRHttpURLConnection.java
@@ -7,7 +7,6 @@ package org.openmrs.module.ugandaemrsync.api;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.ObjectNode;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpEntity;
